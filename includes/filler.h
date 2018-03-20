ac300443bfa41add55cb1700b5fb049e693c4f07
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-19 19:13:54 -0700

    Added player initialization

--- includes/filler.h
+++ includes/filler.h
@@ -1,43 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   filler.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/19 18:47:36 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/19 19:13:00 by ikozlov          ###   ########.fr       */
+/*   Created: 2018/03/19 18:59:15 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/03/19 19:02:19 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "filler.h"
-#include "get_hext_line.h"
+#ifndef FILLER_H
+# define FILLER_H
 
-int		gameon(t_game *game)
+struct					s_game
 {
+	char				**map;
+	char				player;
+	char				opponent;
+};
+typedef struct s_game	t_game;
 
-}
-
-void	whoami(t_game *game)
-{
-	char	*line;
-	
-	if (!get_next_line(STDERR_FILENO, &line))
-		exit(0);
-	line = ft_strchr(line, 'p');
-	if (*(line + 1) == '1')
-		game->player = 'o';
-	else
-		game->player = 'x';
-	free(line);
-}
-
-int		main(void)
-{
-	t_game	game;
-	
-	whoami(&game);
-	while (gameon(&game))
-		continue ;
-	return (0);
-}
+#endif
