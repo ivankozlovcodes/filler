ce913aa854f5d5eaf83e1804b0dcff06e650ea10
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-20 15:03:36 -0700

    Updated header

--- includes/filler.h
+++ includes/filler.h
@@ -6,12 +6,22 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 18:59:15 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/19 21:30:33 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/20 14:46:41 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLER_H
 # define FILLER_H
+
+# include <math.h>
+
+# define PLAYER1 'o'
+# define PLAYER1 'x'
+# define C_POINTS 5
+# define DISTANCE(x1, y1, x2, y2) (sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)))
+
+# define DEBUG 1
+extern int				g_fd;
 
 struct					s_matrix
 {
@@ -31,6 +41,14 @@ struct					s_game
 	t_matrix			map;
 };
 typedef struct s_game	t_game;
+
+struct					s_point
+{
+	int					x;
+	int					y;
+	int					distances[2];
+};
+typedef struct s_point	t_point;
 
 void					get_data(t_game *game);
 int						gameon(t_game *game);
