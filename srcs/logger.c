316f810317105f85abfc22eb3b514b87adb6f6fa
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-22 22:25:36 -0700

    Added logging forcritical points

--- srcs/logger.c
+++ srcs/logger.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/20 16:00:38 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/22 21:32:15 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/22 22:02:00 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,18 @@ void	log_matrix(t_matrix m)
 	ft_log("Size: r%d c%d\n", m.rows, m.cols);
 	while (++i < m.rows)
 		ft_log("%s\n", m.m[i]);
+}
+
+void	log_critical_points(t_point *p)
+{
+	int		i;
+
+	i = -1;
+	ft_log("Critical points\n");
+	while (++i < C_POINTS)
+	{
+		ft_log("[%d, %d]\n", p[i].x, p[i].y);
+		ft_log("%c: %d\t\t", PLAYER1, p[i].distances[0]);
+		ft_log("%c: %d\n", PLAYER2, p[i].distances[1]);
+	}
 }
