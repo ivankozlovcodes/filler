bf7f65d320698a35c2a25269ada4b691f20ce11c
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-22 22:26:51 -0700

    Fixed critical points initialization

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 19:19:06 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/22 21:31:06 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/22 22:26:19 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,22 @@ t_point	*intialize_critical_points(int width, int height)
 	res[0].y = height / 2;
 	res[1].x = 0;
 	res[1].y = 0;
-	res[2].x = width;
+	res[2].x = width - 1;
 	res[2].y = 0;
-	res[3].x = width;
-	res[3].y = height;
+	res[3].x = width - 1;
+	res[3].y = height - 1;
 	res[4].x = 0;
-	res[4].y = height;
+	res[4].y = height - 1;
 	i = -1;
 	while (++i < C_POINTS)
 	{
-		res[i].distances[0] = INT_MAX;
-		res[i].distances[1] = INT_MAX;
+		res[i].distances[0] = width + height;
+		res[i].distances[1] = width + height;
 	}
 	return (res);
 }
 
-void	set_point_owner(t_point *cp, int x, int y, char p)
+void	set_distances(t_point *cp, int x, int y, char p)
 {
 	int		i;
 	int		d;
@@ -49,12 +49,9 @@ void	set_point_owner(t_point *cp, int x, int y, char p)
 	player = ft_tolower(p) == 'x';
 	while (++i < C_POINTS)
 	{
-		d = DISTANCE(cp[i].x, cp[i].y, x, y);
+		d = DISTANCE(x, y, cp[i].x, cp[i].y);
 		if (cp[i].distances[player] > d)
-		{
 			cp[i].distances[player] = d;
-			return ;
-		}
 	}
 }
 
@@ -66,6 +63,7 @@ t_point	*get_critical_points(t_game game)
 	int			j;
 
 	points = intialize_critical_points(game.map.cols, game.map.rows);
+	log_critical_points(points);
 	distance = malloc(sizeof(int) * C_POINTS);
 	i = -1;
 	while (++i < game.map.rows)
@@ -73,7 +71,7 @@ t_point	*get_critical_points(t_game game)
 		j = -1;
 		while (++j < game.map.cols)
 			if (ft_strchr("xo", ft_tolower(game.map.m[i][j])))
-				set_point_owner(points, j, i, game.map.m[i][j]);
+				set_distances(points, j, i, game.map.m[i][j]);
 	}
 	return (points);
 }
@@ -91,14 +89,18 @@ int		gameon(t_game *game)
 {
 	t_point	*critical;
 
-	get_data(game);
-	ft_log("Got data\n");
-	log_matrix(game->map);
-	log_matrix(game->piece.field);
-	critical = get_critical_points(*game);
-	// set main critical point
-	// build fitness matrix
-	// make a move
-	free(critical);
+	while (1)
+	{
+		get_data(game);
+		ft_log("Got data\n");
+		log_matrix(game->map);
+		log_matrix(game->piece.field);
+		critical = get_critical_points(*game);
+		log_critical_points(critical);
+		// set main critical point
+		// build fitness matrix
+		// make a move
+		free(critical);
+	}
 	return (0);
 }
