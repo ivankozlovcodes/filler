f988d94419df465eba1c020cb5f9aaf0e754ac8a
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-26 21:15:09 -0700

    Removed extra stuff

--- srcs/critical_points.c
+++ srcs/critical_points.c
@@ -6,33 +6,13 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 14:55:47 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/25 18:13:53 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/26 21:14:31 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 #include "math.h"
 #include <limits.h>
-
-void	set_distance(t_point point, int x, int y, char p)
-{
-	int		d;
-	int		player;
-
-	player = ft_tolower(p) == PLAYER2;
-	d = DISTANCE(x, y, point.x, point.y);
-	if (point.distances[player] > d)
-		point.distances[player] = d;
-}
-
-void	set_distances(t_point *cp, int x, int y, char p)
-{
-	int		i;
-
-	i = -1;
-	while (++i < C_POINTS)
-		set_distance(cp[i], x, y, p);
-}
 
 t_list	*get_enemy(t_game *game)
 {
@@ -90,17 +70,8 @@ t_point	get_opponents_last_move(t_game *game)
 
 void	set_main_critical_point(t_game *game)
 {
-	int			player;
-	int			opponent;
-	int			distance;
-
-	player = game->player == PLAYER2;
-	opponent = game->opponent == PLAYER2;
-	distance = -1;
 	game->critical_point = get_opponents_last_move(game);
 	game->enemy = get_enemy(game);
 	if (game->critical_point.x < 0 || game->critical_point.y < 0)
 		SETXY(game->critical_point, game->map.cols / 2, game->map.rows / 2);
-	ft_log("Return critical point at pos %d, %d\n", game->critical_point.x,
-		game->critical_point.y);
 }
